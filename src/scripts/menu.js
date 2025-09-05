8bf69cf8407d99e173d67a299a8dce443bc201cf
Youri Lamoureux <yourilamoureux1985@hotmail.com>
2025-09-05 09:45:21 -0600

    fixed: default theme color

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,13 +1,10 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-  // Use the correct ID to find the menu
   const mobileMenu = document.querySelector('#mobile-menu');
   const hamburger = document.querySelector('.hamburger');
 
-  // Toggle the 'expanded' class to show or hide the menu
   if (mobileMenu) {
     mobileMenu.classList.toggle('expanded');
   }
 
-  // Toggle the 'active' class for the hamburger icon
   hamburger.classList.toggle('active');
 });
